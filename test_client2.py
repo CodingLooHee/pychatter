8d1ff7398c8a8b69f0c2497690b94d74b39f818d
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-04-09 14:41:44 +0700

    Add connection error catcher

--- test_client2.py
+++ test_client2.py
@@ -1,4 +1,45 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
+
+
+
+
+
+if __name__ == '__main__':
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    while True:
+        print('Enter host and port you want to connect')
+        HOST = input('Host: ')
+        PORT = input('PORT: ')
+
+        try:
+            if HOST == '':
+                    HOST = '0.0.0.0'
+            if PORT == '':
+                PORT = 12345
+            
+            PORT = int(PORT)
+
+            client.connect((HOST, PORT))
+            break
+        except socket.gaierror:
+            print('Invalid Host address')
+            continue
+        except OSError:
+            print('Invalid Host address')
+            continue
+        except ValueError:
+            print('Port must be a number')
+            continue
+        except OverflowError:
+            print('Port must be 0-65535')
+            continue
+        except Exception as err:
+            print(err)
+    
+
+
+    client.close()
