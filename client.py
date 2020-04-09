b7a0ee036c66d7827a8ad112a9bb76004056b6e5
CodingLooHee <63284510+CodingLooHee@users.noreply.github.com>
2020-04-09 13:14:20 +0700

    Add client.py

--- client.py
+++ client.py
@@ -1,3 +1,12 @@
 import socket
 
-client = socket.socket()
+HOST = '127.0.0.1'
+PORT = 65432
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((HOST, PORT))
+    client.sendall(b'Hello, world')
+    data = client.recv(1024)
+
+print('Received', repr(data))
+input()
